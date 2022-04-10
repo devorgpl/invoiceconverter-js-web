34f3509ae29616dda67f0c41b52c9568ca8181a8
Cyprian <cyprian@sniegota.com>
2022-04-10 23:28:21 +0200

    cypress

---
+++ packages/invoice-web/cypress/integration/convert_spec.js
@@ -0,0 +1,25 @@
+import 'cypress-file-upload';
+import path from 'path';
+
+const deleteDownloadsFolder = () => {
+  const downloadsFolder = Cypress.config('downloadsFolder')
+  cy.task('deleteFolder', downloadsFolder)
+}
+
+beforeEach(deleteDownloadsFolder)
+
+describe('My First Test', () => {
+    it('Does not do much!', () => {
+      cy.visit('http://localhost:5000')
+      const fixtureFile = 'FX.xml';
+      cy.get('#inputfile').attachFile(fixtureFile);
+      const downloadsFolder = Cypress.config('downloadsFolder')
+      const downloadedFilename = path.join(downloadsFolder, 'FX.xml.epp')
+      cy.verifyDownload('FX.xml.epp');
+   
+      cy.readFile(downloadedFilename, 'binary', { timeout: 15000 })
+      .should(buffer => expect(buffer.length).to.be.eq(13643));
+      expect(true).to.equal(true)
+    })
+  })
+  
